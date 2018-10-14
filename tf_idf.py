a98ed290240dd6eefd07062e0b89302a6500bbd6
HakamSophia <hakam.sophia@hotmail.fr>
2018-10-14 19:31:14 +0200

    Add files via upload
    tf.idf

---
+++ tf_idf.py
@@ -0,0 +1,39 @@
+import csv
+import numpy as np
+from collections import Counter
+import itertools
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+def extract_key(v):
+    return v[0]
+
+with open('data.csv', 'w') as file_output:
+    fm = ['Code','Nom']#,'Categorie','tf_idf']
+    wr = csv.DictWriter(file_output,delimiter='\t', fieldnames=fm)
+    wr.writeheader()
+    with open('Living_Beings.csv', 'r') as file_input:
+        rd = csv.reader(file_input, delimiter='\t')
+    ## CSV to Array
+        data = []
+        for row in rd: # each row is a list
+            wr.writerow({'Code': row[0],'Nom': row[1]})
+            data.append(row)
+   #     print data
+    ## Merge the same elements in an array
+        #itertools.groupby needs data to be sorted first
+        data = sorted(data, key=extract_key)
+        result = [[[k],[x[1] for x in g]] for k, g in itertools.groupby(data, extract_key)]
+        print result[0]
+        with open("tfidf.txt","w") as t:
+            for i in range(0,len(result)):
+                corpus = result[i][1]
+                vectorizer = TfidfVectorizer()
+                matrix = vectorizer.fit_transform(corpus)
+           # print("------- {} -------".format(i))
+            #print(matrix)
+                idf=vectorizer._tfidf.idf_
+                p = zip(vectorizer.get_feature_names(), idf)
+                p.sort(key = lambda t: t[1])
+                for x in p:
+                    t.write("------- {} -------".format(result[i][0]))
+                    print>>t, x
