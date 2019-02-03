f486ef7ada479afd8399d37b710d9ce518d7ae44
HakamSophia <hakam.sophia@hotmail.fr>
2019-02-03 19:55:16 +0100

    TF-IDF

--- tf_idf.py
+++ tf_idf.py
@@ -8,7 +8,7 @@ def extract_key(v):
     return v[0]
 
 with open('data.csv', 'w') as file_output:
-    fm = ['Code','Nom']#,'Categorie','tf_idf']
+    fm = ['Code','Nom']#['Categorie','tf_idf']
     wr = csv.DictWriter(file_output,delimiter='\t', fieldnames=fm)
     wr.writeheader()
     with open('Living_Beings.csv', 'r') as file_input:
@@ -18,21 +18,22 @@ with open('data.csv', 'w') as file_output:
         for row in rd: # each row is a list
             wr.writerow({'Code': row[0],'Nom': row[1]})
             data.append(row)
-   #     print data
     ## Merge the same elements in an array
         #itertools.groupby needs data to be sorted first
         data = sorted(data, key=extract_key)
         result = [[[k],[x[1] for x in g]] for k, g in itertools.groupby(data, extract_key)]
-        print result[0]
         with open("tfidf.txt","w") as t:
+            res = []
             for i in range(0,len(result)):
                 corpus = result[i][1]
+                for j in range(0,len(corpus)):
+                    res.append(corpus[j])
                 vectorizer = TfidfVectorizer()
-                matrix = vectorizer.fit_transform(corpus)
-                feature_names = vectorizer.get_feature_names()
-                doc = 0
-                feature_index = matrix[doc,:].nonzero()[1]
-                tfidf_scores = zip(feature_index, [matrix[doc, x] for x in feature_index])
-                for w, s in [(feature_names[i], s) for (i, s) in tfidf_scores]:
-                    t.write("------- {} -------".format(result[i][0]))
-                    print >> t, (w, s)
+                matrix = vectorizer.fit_transform(res)
+            ##Here the tuple represents, document no. (in this case sentence no.) and feature no.
+            t.write(" ------- Tf-idf -------\n")
+            print >> t, matrix
+            t.write("\n\n\n ------- List of features -------\n")
+            ##List of features
+            for i, feature in enumerate(vectorizer.get_feature_names()):
+                print >> t, (i,feature)
